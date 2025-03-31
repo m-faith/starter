279e5c03ee7443b32d48233624f37591a1cd860c
m-faith <123572922+m-faith@users.noreply.github.com>
2025-03-31 00:02:24 -0400

    Be extra explicit in example.lua regarding empty spec.

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,7 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
 if true then return {} end
+-- deleting that line would allow the following code to load...
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
